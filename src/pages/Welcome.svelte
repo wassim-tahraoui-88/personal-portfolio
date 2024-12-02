39246529cf119ab0ba66736c45927dd68ba24b44
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2024-12-02 14:57:22 +0100

    - Set up a page system with a bootstrap-like stylesheet in app.html

---
+++ src/pages/Welcome.svelte
@@ -0,0 +1,9 @@
+<style lang="scss">
+    #_root {
+        background: red;
+    }
+</style>
+<div id="_root" class="container row">
+    <div class="container column">Foo</div>
+    <div class="container column">Bar</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,18 @@
 <script lang="ts">
-    import Cursor from '../component/Cursor.svelte';
-    import Button from '../component/Button.svelte';
+    import Welcome from '../pages/Welcome.svelte';
     import { onMount } from 'svelte';
-    let paneOverlay : Element, paneBase : Element;
+    // let paneOverlay : Element, paneBase : Element;
 
     onMount(() => {});
 </script>
 <style lang="scss">
-    .stack-pane {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-    }
-    .pane {
-        position: absolute;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        width: 100%;
-        height: 100%;
-    }
-    .container {
-	    position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        gap: 1rem;
-    }
 </style>
-<div class="stack-pane">
-    <div bind:this={paneBase} class="pane main">
-        <div class="container">
-<!--            <Button text="Hello World"/>-->
-        </div>
-    </div>
-    <div bind:this={paneOverlay} style="pointer-events: none;" class="pane overlay">
-        <Cursor/>
-    </div>
+<div id="_root" class="container">
+    <Welcome/>
+<!--    <div bind:this={paneBase} class="pane main">-->
+
+<!--    </div>-->
+<!--    <div bind:this={paneOverlay} style="pointer-events: none;" class="pane overlay">-->
+<!--        <Cursor/>-->
+<!--    </div>-->
 </div>
