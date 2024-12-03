74d02a64bf34d4bb88ae1706bfdd1d9a3e82ebc6
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2024-12-04 00:50:39 +0100

    - Adding Transition logic.

--- src/utils/Transitions.ts
+++ src/utils/Transitions.ts
@@ -1,11 +1,12 @@
 export type TransitionOptions = { transitionTime : string, isEnter : boolean }
+export type ConsumerFunction = (element : Element, options : TransitionOptions) => void
 
 export enum TransitionType {
 	FADE, SWIPE
 }
 
 
-const Transitions : { [key in TransitionType] : { setup: Function, apply : Function } } = {
+const Transitions : { [key in TransitionType] : { setup: ConsumerFunction, apply : ConsumerFunction } } = {
 	[TransitionType.FADE]: {
 		setup: (element : Element, options : TransitionOptions) => {
 			// TODO: Necessary setup logic here
